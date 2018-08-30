bdc733f250b1b1302d70228c491ddd42b4a9f4f7
Adrien PUSEL <adpusel@student.42.fr>
2018-08-30 12:34:57 +0200

    save

---
+++ res/adpusel/file/test/test_address.c
@@ -0,0 +1,21 @@
+#include "../includes/ft_asm_header.h"
+
+#pragma clang diagnostic push
+/*------------------------------------*\
+	RET TAB
+\*------------------------------------*/
+int res[4] = {0};
+
+t_dll g_list;
+t_dll_l link_tab[4];
+char *label_tab[4][10] = {
+	"label:",
+	"super:",
+	"manger:",
+	"qwery:",
+
+
+void init_test_list()
+{
+
+}
