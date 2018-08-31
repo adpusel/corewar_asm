200b6ac738fb3f7cfe04292513bf6b405633becd
Adrien PUSEL <adpusel@student.42.fr>
2018-08-31 16:22:37 +0200

    get_op est ok et testé, il marche avec des DEFINE dans a_function, il return un char

--- res/adpusel/file/test/test_get_op.c
+++ res/adpusel/file/test/test_get_op.c
@@ -1,20 +1,29 @@
 #include "ft_asm_header.h"
 
-static char *return_good_label()
+static char *test_res()
 {
-	int p[3] = {REGISTRE, DIRECT, DIRECT};
-	unsigned char res;
+	int p[3] = { REGISTRE, DIRECT, DIRECT };
+	int p1[3] = { REGISTRE, DIRECT, REGISTRE };
+	int p2[3] = { INDIRECT, 0, 0 };
+	unsigned char res_1;
+	unsigned char res_2;
+	unsigned char res_3;
 
-	res = get_op(p[0], p[1], p[2]);
+	res_1 = get_op(p[0], p[1], p[2]);
+	res_2 = get_op(p1[0], p1[1], p1[2]);
+	res_3 = get_op(p2[0], p2[1], p2[2]);
 
-
-//	TEST("error --> label start good 3",
-//		 STR_EQ(str_ret_3, label) && ret_3 == TRUE);
+	TEST("error --> get_op_test 1",
+		 res_1 == 0x68);
+	TEST("error --> get_op_test 2",
+		 res_2 == 0x64);
+	TEST("error --> get_op_test 3",
+		 res_3 == 0xC0);
 	return NULL;
 }
 
 char *all_get_op_test()
 {
-	mu_run_test(return_good_label);
+	mu_run_test(test_res);
 	return 0;
 }
