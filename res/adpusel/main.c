12f66af47f7f8ff0c34e6bbdd1774f8153e92fcc
get <adrien.pusel@gmail.com>
2018-09-01 14:13:04 +0200

    delete le cast dans parse_op, deja char*

--- res/adpusel/main.c
+++ res/adpusel/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "file/test/all_test.h"
 
-int main()
-{
+//int main()
+//{
 //	static char ok[5] = {COMMENT_CHAR, LABEL_CHAR};
-	all_test();
-
-	return 0;
-}
+//	all_test();
+//
+//	return 0;
+//}
